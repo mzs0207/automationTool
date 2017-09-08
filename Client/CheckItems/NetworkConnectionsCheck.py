a1c812facb6efbb72262ffc59336a248b331fc62
mzs <378077287@qq.com>
2017-09-08 15:56:21 +0800

    修复NetworkConnectionsCheck bug

--- Client/CheckItems/NetworkConnectionsCheck.py
+++ Client/CheckItems/NetworkConnectionsCheck.py
@@ -19,6 +19,8 @@ class NetworkConnections(Interface):
 
         for item in connections:
             c_state = item.status
+            if len(item.raddr) == 0:
+                continue
             r_ip = item.raddr.ip
 
             if c_state in state:
@@ -31,7 +33,10 @@ class NetworkConnections(Interface):
             else:
                 remote_hosts[r_ip] = 1
 
-        remote_hosts_10 = heapq.nlargest(10, remote_hosts, key= lambda k: remote_hosts[k])
+        ip_10 = heapq.nlargest(10, remote_hosts, key= lambda k: remote_hosts[k])
+        remote_hosts_10 = []
+        for ip in ip_10:
+            remote_hosts_10.append({'ip':ip, 'count':remote_hosts[ip]})
         data['hosts'] = remote_hosts_10
         data['status'] = state
 
@@ -40,4 +45,4 @@ class NetworkConnections(Interface):
 
 if __name__ == '__main__':
     conn = NetworkConnections()
-    conn.check()
+    print conn.check()
