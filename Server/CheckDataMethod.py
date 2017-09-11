aa7b6a764ebf8b53a8c6caa06c3800b5a86642b3
mengzengshan <378077287@qq.com>
2017-09-11 18:16:42 +0800

    开始写后台接收数据接口

---
+++ Server/CheckDataMethod.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding:utf8
+#
+#   处理客户端传过来的检查项数据
+#   一  解密并验证客户端token
+#   二  数据入库
+#   三  判断是否到达报警阈值并发送报警
+#
+#
+from config import config
+
+def process(json_data):
+    pass
