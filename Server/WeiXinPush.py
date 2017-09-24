73e0d85ab9a544ebe567a557c4d25b1cb5fc8f8e
mzs <378077287@qq.com>
2017-09-24 16:14:15 +0800

    微信调试成功

--- Server/WeiXinPush.py
+++ Server/WeiXinPush.py
@@ -6,13 +6,14 @@ import urllib2
 import datetime
 from config import config
 import MySQLdb
+import traceback
 
 # 获取公众号token的接口
 WX_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
 # 模板消息推送接口
 WX_TEMPLATE_MSG_API = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s"
 WX_TEMPLATE_ID=config['WX_TEMPLATE_ID']
-print WX_TEMPLATE_ID
+#print WX_TEMPLATE_ID
 
 WX_APPID = config['WX_APPID']
 WX_SECRET = config['WX_SECRET']
@@ -20,7 +21,7 @@ USERS=[{'userid':'18621210266','openid':'ofQzPvz-ULZFnvKydittC8hTAGSk'}]
 
 
 def get_token():
-    return 'IzDalt8zGy71PEsg57DPcOgzAFMke_gZqJ6H_iD2iIBahHHY6BbYf4L88ps3FttXoeRZavq9TwivoSCIUCRfmr9X7Qt6nwfMFdWSlnslWCYHOQfRITgykykWKOtO829ROLWdACAROZ'
+    #return 'IzDalt8zGy71PEsg57DPcOgzAFMke_gZqJ6H_iD2iIBahHHY6BbYf4L88ps3FttXoeRZavq9TwivoSCIUCRfmr9X7Qt6nwfMFdWSlnslWCYHOQfRITgykykWKOtO829ROLWdACAROZ'
     url=WX_TOKEN_URL %(WX_APPID,WX_SECRET)
     try:
         request=urllib2.Request(url)
@@ -34,25 +35,39 @@ def get_token():
 
 def get_token_form_db():
     try:
-        conn = MySQLdb.connect(host = config['WX_MySQL_Host'], port = config['WX_MySQL_Port'],
-                               user= config['WX_MySQL_User'], passwd = config['WX_MySQL_Passwd'])
+        #print 'run get_token_form_db'
+        #print 'Host:',config['WX_MySQL_Host']
+        #print 'Port:',config['WX_MySQL_Port']
+        #print 'User:',config['WX_MySQL_User']
+        #print 'Passwd:',config['WX_MySQL_Passwd']
+        conn = MySQLdb.connect(host = config['WX_MySQL_Host'], 
+                               port = config['WX_MySQL_Port'],
+                               user= config['WX_MySQL_User'],
+                               passwd = config['WX_MySQL_Passwd'],
+                               db='stock_app')
         cursor = conn.cursor()
+        #print 'sql:',config['WX_SQL']
         cursor.execute(config['WX_SQL'])
         data = cursor.fetchone()
         cursor.close()
         conn.close()
+        #print 'db token:',data[0]
         return data[0]
     except Exception,e:
         print e
+        traceback.print_exc()
 
 
 def send_weixin_msg( title,content,remark,users=USERS):
     if len(users) == 0:
         return
     if config['WX_Token_Source'] == 'db':
+        #print 'source from db'
         token = get_token_form_db()
     else:
+        #print 'source from get'
         token= get_token()
+    #print 'token:',token
     time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     template_msg = {
         "first": {
